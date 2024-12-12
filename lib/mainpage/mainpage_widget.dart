1867461e4f410d9596f4416c78af1d689aa5de33
eng <ruddbs2817@gmail.com>
2024-12-12 23:23:20 +0900

    ui 코드와 합치기

--- lib/mainpage/mainpage_widget.dart
+++ lib/mainpage/mainpage_widget.dart
@@ -103,221 +103,167 @@ class _MainpageWidgetState extends State<MainpageWidget> {
                 child: Column(
                   mainAxisSize: MainAxisSize.max,
                   children: [
-                    Padding(
-                      padding:
-                      const EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 8.0),
-                      child: Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color:
-                          FlutterFlowTheme.of(context).secondaryBackground,
-                          boxShadow: const [
-                            BoxShadow(
-                              blurRadius: 3.0,
-                              color: Color(0x411D2429),
-                              offset: Offset(
-                                0.0,
-                                1.0,
-                              ),
-                            )
-                          ],
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(5.0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    20.0, 20.0, 0.0, 0.0),
-                                child: Text(
-                                  '물품 검색',
-                                  style: FlutterFlowTheme.of(context)
-                                      .headlineSmall
-                                      .override(
-                                    fontFamily: 'Pretendard',
-                                    letterSpacing: 0.0,
-                                    useGoogleFonts: false,
-                                  ),
+                    InkWell(
+                      onTap: () {
+                        print('실험기구 검색창으로 이동하는 버튼_mainpage');
+                        context.push('/devicesearch');
+                      },
+                      child: Padding(
+                        padding:
+                        const EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 8.0),
+                        child: Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color:
+                            FlutterFlowTheme.of(context).secondaryBackground,
+                            boxShadow: const [
+                              BoxShadow(
+                                blurRadius: 3.0,
+                                color: Color(0x411D2429),
+                                offset: Offset(
+                                  0.0,
+                                  1.0,
                                 ),
-                              ),
-                              const SizedBox(height: 3,),
-                              Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    20.0, 8.0, 8.0, 0.0),
-                                child: AutoSizeText(
-                                  '필요한 물품 검색하여 대여 신청',
-                                  textAlign: TextAlign.start,
-                                  style: FlutterFlowTheme.of(context)
-                                      .titleSmall
-                                      .override(
-                                    fontFamily: 'Pretendard',
-                                    letterSpacing: 0.0,
-                                    useGoogleFonts: false,
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    8.0, 0.0, 8.0, 0.0),
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.max,
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsetsDirectional.fromSTEB(
-                                          0.0, 8.0, 4.0, 8.0),
-                                      child: Text(
-                                        '화살표 눌러서 이동하기',
-                                        textAlign: TextAlign.end,
-                                        style: FlutterFlowTheme.of(context)
-                                            .labelSmall
-                                            .override(
-                                          fontFamily: 'Pretendard',
-                                          letterSpacing: 0.0,
-                                          useGoogleFonts: false,
-                                        ),
-                                      ),
+                              )
+                            ],
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsetsDirectional.fromSTEB(
+                                      20.0, 20.0, 0.0, 0.0),
+                                  child: Text(
+                                    '물품 검색',
+                                    style: FlutterFlowTheme.of(context)
+                                        .headlineSmall
+                                        .override(
+                                      fontFamily: 'Pretendard',
+                                      letterSpacing: 0.0,
+                                      useGoogleFonts: false,
                                     ),
-                                    InkWell(
-                                      onTap: (){
-                                        print('실험기구 검색창으로 이동하는 버튼_mainpage');
-                                        context.push('/devicesearch');
-                                      },
-                                      child: const Icon(
+                                  ),
+                                ),
+                                const SizedBox(height: 3,),
+                                Padding(
+                                  padding: const EdgeInsetsDirectional.fromSTEB(
+                                      20.0, 8.0, 8.0, 0.0),
+                                  child: AutoSizeText(
+                                    '필요한 물품 검색하여 대여 신청',
+                                    textAlign: TextAlign.start,
+                                    style: FlutterFlowTheme.of(context)
+                                        .titleSmall
+                                        .override(
+                                      fontFamily: 'Pretendard',
+                                      letterSpacing: 0.0,
+                                      useGoogleFonts: false,
+                                    ),
+                                  ),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      8.0, 0.0, 8.0, 0.0),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Icon(
                                         Icons.chevron_right_rounded,
                                         color: Color(0xFF57636C),
                                         size: 30.0,
                                       ),
-
-                                    )
-                                  ],
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding:
-                      const EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 8.0),
-                      child: Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color:
-                          FlutterFlowTheme.of(context).secondaryBackground,
-                          boxShadow: const [
-                            BoxShadow(
-                              blurRadius: 3.0,
-                              color: Color(0x411D2429),
-                              offset: Offset(
-                                0.0,
-                                1.0,
-                              ),
-                            )
-                          ],
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    20.0, 20.0, 0.0, 0.0),
-                                child: Text(
-                                  '대여 현황',
-                                  style: FlutterFlowTheme.of(context)
-                                      .headlineSmall
-                                      .override(
-                                    fontFamily: 'Pretendard',
-                                    letterSpacing: 0.0,
-                                    useGoogleFonts: false,
-                                  ),
+                    InkWell(
+                      onTap: () {
+                        print('신청 목록으로 이동하는 버튼_mainpage');
+                        context.push('/borrowlabel');
+                      },
+                      child: Padding(
+                        padding:
+                        const EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 8.0),
+                        child: Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color:
+                            FlutterFlowTheme.of(context).secondaryBackground,
+                            boxShadow: const [
+                              BoxShadow(
+                                blurRadius: 3.0,
+                                color: Color(0x411D2429),
+                                offset: Offset(
+                                  0.0,
+                                  1.0,
                                 ),
-                              ),
-                              const SizedBox(height: 3,),
-                              Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    20.0, 8.0, 8.0, 0.0),
-                                child: AutoSizeText(
-                                  '대여한 물품 확인',
-                                  textAlign: TextAlign.start,
-                                  style: FlutterFlowTheme.of(context)
-                                      .titleSmall
-                                      .override(
-                                    fontFamily: 'Pretendard',
-                                    letterSpacing: 0.0,
-                                    useGoogleFonts: false,
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    8.0, 0.0, 8.0, 0.0),
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.max,
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsetsDirectional.fromSTEB(
-                                          0.0, 8.0, 4.0, 8.0),
-                                      child: Text(
-                                        '화살표 눌러서 이동하기',
-                                        textAlign: TextAlign.end,
-                                        style: FlutterFlowTheme.of(context)
-                                            .labelSmall
-                                            .override(
-                                          fontFamily: 'Pretendard',
-                                          letterSpacing: 0.0,
-                                          useGoogleFonts: false,
-                                        ),
-                                      ),
+                              )
+                            ],
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsetsDirectional.fromSTEB(
+                                      20.0, 20.0, 0.0, 0.0),
+                                  child: Text(
+                                    '대여 현황',
+                                    style: FlutterFlowTheme.of(context)
+                                        .headlineSmall
+                                        .override(
+                                      fontFamily: 'Pretendard',
+                                      letterSpacing: 0.0,
+                                      useGoogleFonts: false,
                                     ),
-                                    InkWell(
-                                      onTap: (){
-                                        print('신청 목록으로 이동하는 버튼_mainpage');
-                                        context.push('/borrowlabel');
-                                      },
-                                      child: const Icon(
+                                  ),
+                                ),
+                                const SizedBox(height: 3,),
+                                Padding(
+                                  padding: const EdgeInsetsDirectional.fromSTEB(
+                                      20.0, 8.0, 8.0, 0.0),
+                                  child: AutoSizeText(
+                                    '대여한 물품 확인',
+                                    textAlign: TextAlign.start,
+                                    style: FlutterFlowTheme.of(context)
+                                        .titleSmall
+                                        .override(
+                                      fontFamily: 'Pretendard',
+                                      letterSpacing: 0.0,
+                                      useGoogleFonts: false,
+                                    ),
+                                  ),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      8.0, 0.0, 8.0, 0.0),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Icon(
                                         Icons.chevron_right_rounded,
                                         color: Color(0xFF57636C),
                                         size: 30.0,
                                       ),
-
-                                    )
-                                  ],
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                      const EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 8.0),
-                      child: Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color:
-                          FlutterFlowTheme.of(context).secondaryBackground,
-                          boxShadow: const [
-                            BoxShadow(
-                              blurRadius: 3.0,
-                              color: Color(0x411D2429),
-                              offset: Offset(
-                                0.0,
-                                1.0,
-                              ),
-                            )
-                          ],
-                          borderRadius: BorderRadius.circular(8.0),
                         ),
                       ),
                     ),
