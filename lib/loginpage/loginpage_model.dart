0a70d4c5d2dc02c6d3789a5df0239c7093a1de12
eng <ruddbs2817@gmail.com>
2024-12-15 16:17:06 +0900

    로그인, 회원가입에서 에러 관리

--- lib/loginpage/loginpage_model.dart
+++ lib/loginpage/loginpage_model.dart
@@ -52,29 +52,32 @@ class LoginCreateAccountPageModel
       await createAccount(emailAddressCreateTextController.text, passwordCreateTextController.text);
       return true;
     } on FirebaseAuthException catch (e) {
-      switch (e.code) {
-        case 'invalid-email':
-          showAlertWithoutChoice(context, '잘못된 이메일 형식입니다');
-          break;
-        case 'user-not-found' || 'wrong-password' || 'invalid-credential':
-          showAlertWithoutChoice(context, '이메일 혹은 비밀번호가 일치하지 않습니다');
-          break;
-        case 'missing-password':
-          showAlertWithoutChoice(context, '비밀번호를 입력하세요');
-          break;
-        case 'email-already-in-use':
-          showAlertWithoutChoice(context, '이미 해당 계정으로 가입한 계정이 있습니다');
-          break;
-        case 'weak-password':
-          showAlertWithoutChoice(context, '비밀번호는 6자리 이상이어야 합니다');
-          break;
-        default:
-          showAlertWithoutChoice(context, '에러가 발생했습니다. 관리자에게 문의하세요');
+      if(context.mounted) {
+        switch (e.code) {
+          case 'invalid-email':
+            showAlertWithoutChoice(context, '잘못된 이메일 형식입니다');
+            break;
+          case 'user-not-found' || 'wrong-password' || 'invalid-credential':
+            showAlertWithoutChoice(context, '이메일 혹은 비밀번호가 일치하지 않습니다');
+            break;
+          case 'missing-password':
+            showAlertWithoutChoice(context, '비밀번호를 입력하세요');
+            break;
+          case 'email-already-in-use':
+            showAlertWithoutChoice(context, '이미 해당 계정으로 가입한 계정이 있습니다');
+            break;
+          case 'weak-password':
+            showAlertWithoutChoice(context, '비밀번호는 6자리 이상이어야 합니다');
+            break;
+          default:
+            showAlertWithoutChoice(context, '에러가 발생했습니다. 관리자에게 문의하세요');
+        }
       }
-      debugPrint(e.code);
-      debugPrint(e.toString());
       return false;
     } catch(e) {
+      if(context.mounted) {
+        showAlertWithoutChoice(context, '에러가 발생했습니다. 관리자에게 문의하세요');
+      }
       return false;
     }
   }
@@ -84,28 +87,29 @@ class LoginCreateAccountPageModel
       await signInWithEmail(emailAddressTextController.text, passwordTextController.text);
       return true;
     } on FirebaseAuthException catch (e) {
-      switch (e.code) {
-        case 'invalid-email':
-          showAlertWithoutChoice(context, '잘못된 이메일 형식입니다');
-          break;
-        case 'user-not-found' || 'wrong-password' || 'invalid-credential':
-          showAlertWithoutChoice(context, '이메일 혹은 비밀번호가 일치하지 않습니다');
-          break;
-        case 'missing-password':
-          showAlertWithoutChoice(context, '비밀번호를 입력하세요');
-          break;
-        case 'user-disabled':
-          showAlertWithoutChoice(context, '해당 계정이 비활성화 상태입니다');
-          break;
-        default:
-          showAlertWithoutChoice(context, '에러가 발생했습니다. 관리자에게 문의하세요');
+      if(context.mounted) {
+        switch (e.code) {
+          case 'invalid-email':
+            showAlertWithoutChoice(context, '잘못된 이메일 형식입니다');
+            break;
+          case 'user-not-found' || 'wrong-password' || 'invalid-credential':
+            showAlertWithoutChoice(context, '이메일 혹은 비밀번호가 일치하지 않습니다');
+            break;
+          case 'missing-password':
+            showAlertWithoutChoice(context, '비밀번호를 입력하세요');
+            break;
+          case 'user-disabled':
+            showAlertWithoutChoice(context, '해당 계정이 비활성화 상태입니다');
+            break;
+          default:
+            showAlertWithoutChoice(context, '에러가 발생했습니다. 관리자에게 문의하세요');
+        }
       }
-      debugPrint(e.code);
-      debugPrint(e.toString());
       return false;
     }catch(e) {
-      showAlertWithoutChoice(context, '에러가 발생했습니다. 관리자에게 문의하세요');
-      debugPrint(e.toString());
+      if(context.mounted) {
+        showAlertWithoutChoice(context, '에러가 발생했습니다. 관리자에게 문의하세요');
+      }
       return false;
     }
   }
