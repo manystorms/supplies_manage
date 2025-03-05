31133332b203ba784f966c5252cf555b4838e030
eng <ruddbs2817@gmail.com>
2025-03-06 00:48:36 +0900

    계정 삭제 페이지 수정

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'application',
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
